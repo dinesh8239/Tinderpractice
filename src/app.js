e8692cb32bf4a2f84f2d642e1dd7600406c99795
Your Name <your_email@example.com>
2024-11-04 22:24:09 +0530

    playing with Routes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,35 @@
 const express = require('express')
 const app = express()
 
-app.get('/test',(req, res) => {
-    res.send('data keep successfully in db')
-})
+app.use('/test',(req, res, next) => {
 
-app.post('/test', (req, res) => {
-res.send('data get successfully')
-})
+    console.log('your api is working 1');
+    // res.send('Router handler 1')
+    next()
+},
+[(req, res, next) =>{
+    console.log('your api is working 2');
+    // res.send('Router handler 2')
+    next()
+},
+(req, res, next) => {
+console.log('your api is working 3');
+// res.send('Router handler 3')
+next()
+},
 
-app.delete('/test',(req, res) => {
-    res.send('deleted successfully')
-})
+(req, res, next) => {
+    console.log('your api is working 4');
+    res.send('Router handler 4')
+    next()
+}],
+
+(req, res, next) => {
+console.log('your api is working 5');
+// res.send('Router handler 5')
+// next()
+}
+)
 
 app.listen(5000, () => {
     console.log('server is running at port 5000');
