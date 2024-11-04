dcea12a1a952dc7d269573cf04be9fc06abc5dfe
Your Name <your_email@example.com>
2024-11-04 18:59:26 +0530

    get, post, delete

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const app = express()
 
+
 app.use("/pest", (req, res) => {
     res.send('mere mehaboob')
 })
 
+app.use("/test2", (req, res) => {
+    res.send('All is good2')
+})
 app.use("/test", (req, res) => {
     res.send('All is well')
 })
 
+
 app.use("/", (req, res) => {
+    
     res.send('Data successfully get')
 })
-
 
 app.listen(5000); 
